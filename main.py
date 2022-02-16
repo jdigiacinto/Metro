c1b3251a0df442b61fd1a9b1ad8a34b75d062f57
jdigiacinto <oahujimmie@gmail.com>
2022-02-16 14:30:09 -0600

    Update main.py
    added more to code, still not working

--- main.py
+++ main.py
@@ -10,27 +10,45 @@ encryption and other methods to verify confidentiality and file integrity.
 '''
 #PACKAGE IMPORTS
 
+
 #WELCOME TO METRO
-print("Welcome to Metro LDAP file management client.%n")
+print("Welcome to Metro LDAP file management client.\n")
 
 
 #PROMPT USER FOR THE SERVER IP ADDRESS
-serverIP = string(input("Please specify the IP address of the server you're accessing:  "))
+serverIP = input("Please specify the IP address of the server you're accessing:  ")
+
+
+validIP = False
+
+#CHECKS IF SUPPLIED IP ADDRESS IS VALID
+def checkIP(ip):
+    try:
+        octets = ip.split('.')
+        n = 0
+        
+        if(len(octets) == 4):
+            for octet in octets:
+                validIP = True if (int(octet) >= 0 
+                                   and int(octet) < 256) else False
+                n += 1
+            return validIP 
+    
+    except ValueError:
+        return False # at least one octet cannot be converted to int.
+    
+    except (AttributeError, TypeError):
+        return False # address is not a string.
+
+
+checkIP(serverIP)
 
 
 #VERIFY THAT THE IP ADDRESS IS VALID, THEN PRINT STATUS MESSAGE
-if(checkIP(serverIP)):
+if(validIP == True):
     print("Accessing LDAP server " + serverIP)
 else:
-    print("Error. Please enter a valid IP address.%n")
+    print("Error. Please enter a valid IP address.\n")
 
 
-#CHECKS IF SUPPLIED IP ADDRESS IS VALID
-def checkIP(address):
-    try:
-        octets = ip.split('.')
-        return len(octets) == 4 and all(0 <= int(octets) < 256 for octet in octets)
-    except ValueError:
-        return False # at least one octet cannot be converted to int.
-    except (AttributeError, TypeError):
-        return False # address is not a string.
+
