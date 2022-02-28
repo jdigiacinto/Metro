70b4e7ad496b602223ee9eefddee7a1af72c57e9
jdigiacinto <oahujimmie@gmail.com>
2022-02-28 11:04:49 -0600

    Update main.py
    worked on main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ Authors: Jimmie DiGiacinto, Joseph Teniente, Christian Degollado,
 Nikolaus Herrera, Grant Carrell
 
 Purpose: To allow the user to connect to a specified LDAP server and
-upload, download, delete, and move files. The client will incorporate 
-end-to-end encryption and other methods to verify confidentiality and 
+upload, download, delete, and move files. The client will incorporate
+end-to-end encryption and other methods to verify confidentiality and
 file integrity.
 '''
 #PACKAGE IMPORTS
-
+import ldap
 
 #WELCOME TO METRO
 print("Welcome to Metro LDAP file management client.\n")
@@ -25,34 +25,48 @@ def promptIP():
 validIP = False
 
 #CHECKS IF SUPPLIED IP ADDRESS IS VALID
+'''
 def checkIP(ip):
     try:
         octets = ip.split('.')
         n = 0
-        
+
         if(len(octets) == 4):
             for octet in octets:
-                validIP = True if (int(octet) >= 0 
+                validIP = True if (int(octet) >= 0
                                    and int(octet) < 256) else False
                 n += 1
-            return validIP 
-    
+            return validIP
+
     except ValueError:
         return False # at least one octet cannot be converted to int.
-    
+
     except (AttributeError, TypeError):
         return False # address is not a string.
 
 while True:
     serverIP = promptIP()
     checkIP(serverIP)
-
+'''
 
 #VERIFY THAT THE IP ADDRESS IS VALID, THEN PRINT STATUS MESSAGE
+'''
 if(validIP == True):
     print("Accessing LDAP server " + serverIP)
 else:
     print("Error. Please enter a valid IP address.\n")
+'''
 
+def main():
+    server = str(promptIP())
+    who = ""
+    cred = ""
 
+    keyword = "metro"
 
+    try:
+        l = ldap.open(server)
+        l.simple_bind_s(who, cred)
+        print("Successfully bound to server.\n")
+    except:
+        print("uh oh")
